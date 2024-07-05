a16774b4c83067a3f72a6e74176c765e0424050d
Honi2302 <129046156+Honi2302@users.noreply.github.com>
2024-07-05 11:26:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
-const allowedOrigins = ['http://localhost:3000', 'https://icms2024-honi2302s-projects.vercel.app'];
+const allowedOrigins = ['http://localhost:3000'];
 
 app.use(cors({
     origin: allowedOrigins,
